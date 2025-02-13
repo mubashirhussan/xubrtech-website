3037a918fcf81e9d7fd36e92152a31af829ae782
mubashirhussan <mubashirhussan93@gmail.com>
2025-02-13 20:59:41 +0500

    hero scetion added

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  sassOptions: {
+    includePaths: ["./src/styles"],
+  },
 };
 
 export default nextConfig;
