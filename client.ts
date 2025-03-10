2e8978896f9e32fd71c7e06a435a73178d394236
mubashirhussan <mubashirhussan93@gmail.com>
2025-03-10 19:12:19 +0500

    project id issue resolved during build

--- client.ts
+++ client.ts
@@ -1,10 +1,10 @@
 import { createClient } from "@sanity/client";
-
+console.log("Sanity Project ID:", process.env.NEXT_PUBLIC_SANITY_PROJECT_ID);
 const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET, // or the name you chose in step 1
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "3aauiptw",
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production", // or the name you chose in step 1
   apiVersion: "2024-07-01",
-  useCdn: true, // `false` if you want to ensure fresh data
+  useCdn: false, // `false` if you want to ensure fresh data
 });
 
 export default client;
